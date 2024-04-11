a8447003d0526f79879542ee279a78271595c0e7
Rutuja More <113405597+morerutuja@users.noreply.github.com>
2024-04-11 17:37:09 -0400

    Update pinecone_upload.py

--- Step1_LOS_TechNote/streamlit/pinecone_upload.py
+++ Step1_LOS_TechNote/streamlit/pinecone_upload.py
@@ -11,8 +11,8 @@ import unicodedata
 
 
 # Connect to Pinecone
-#pinecone_api_key = os.getenv("pinecone_api_key")
-pinecone = Pinecone(api_key= "856f2cfc-fab8-4805-b3b6-690870a383f7")
+pinecone_api_key = os.getenv("pinecone_api_key")
+pinecone = Pinecone(api_key= pinecone_api_key)
 
 # Load pre-trained model for vectorization (replace with your chosen model)
 model = SentenceTransformer('all-MiniLM-L6-v2')
@@ -46,37 +46,42 @@ def prepare_data(row):
         }
     }
 
-# Read CSV files
-csv_files = ['EquityValuationConceptsandBasicTools.csv', 'Introduction to Industry and Company Analysis.csv', 'Market Efficiency.csv']
+def main():
+    # Read CSV files
+    csv_files = ['EquityValuationConceptsandBasicTools.csv', 'Introduction to Industry and Company Analysis.csv', 'Market Efficiency.csv']
 
-# Directory containing CSV files
-csv_data_path = 'data/csv_files'
+    # Directory containing CSV files
+    csv_data_path = 'data/csv_files'
 
-data = {}
+    data = {}
 
-# Loop through each CSV file
-for file in csv_files:
-    with open(os.path.join(csv_data_path, file), 'r', newline='', encoding='utf-8') as csvfile:
-        csvreader = csv.DictReader(csvfile)
-        for row in csvreader:
-            prepared_data = prepare_data(row)
-            topic = row['Topic']
-            # Remove non-ASCII characters from topic
-            topic = ''.join(char for char in topic if unicodedata.category(char)[0] == 'L')
-            if topic not in data:
-                data[topic] = [prepared_data]  # Store data as a list
-            else:
-                data[topic].append(prepared_data) 
+    # Loop through each CSV file
+    for file in csv_files:
+        with open(os.path.join(csv_data_path, file), 'r', newline='', encoding='utf-8') as csvfile:
+            csvreader = csv.DictReader(csvfile)
+            for row in csvreader:
+                prepared_data = prepare_data(row)
+                topic = row['Topic']
+                # Remove non-ASCII characters from topic
+                topic = ''.join(char for char in topic if unicodedata.category(char)[0] == 'L')
+                if topic not in data:
+                    data[topic] = [prepared_data]  # Store data as a list
+                else:
+                    data[topic].append(prepared_data) 
 
-# Save data dictionary to a JSON file
-json_data_path = 'data/json_files'  # Directory for JSON files
-os.makedirs(json_data_path, exist_ok=True)  # Create directory if not exists
+    # Save data dictionary to a JSON file
+    json_data_path = 'data/csv_files'  # Directory for JSON files
+    os.makedirs(json_data_path, exist_ok=True)  # Create directory if not exists
 
-for topic, info in data.items():
-    with open(os.path.join(json_data_path, f'{topic}.json'), 'w', encoding='utf-8') as jsonfile:
-        json.dump(info, jsonfile, indent=4)
+    for topic, info in data.items():
+        with open(os.path.join(json_data_path, f'{topic}.json'), 'w', encoding='utf-8') as jsonfile:
+            json.dump(info, jsonfile, indent=4)
 
 
-for topic, documents in data.items():
-    index.upsert(documents, namespace=topic)
-print("Data uploaded successfully to Pinecone!")
+    for topic, documents in data.items():
+        index.upsert(documents, namespace=topic)
+    print("Data uploaded successfully to Pinecone!")
+
+
+if __name__ == "__main__":
+    main()
